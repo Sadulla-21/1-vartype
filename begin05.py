e88168afa8f7362d890531d50256e1afeacf1075
Sadulla -21~ <hayotolim2022@gmail.com>
2022-12-25 15:57:17 +0500

    fifth

--- begin05.py
+++ begin05.py
@@ -1,7 +1,9 @@
 # Assign 7 (int value) to 'a' variable;
-
+a=7
 # Assign 3 (int value) to 'b' variable;
-
+b=3
 # Assign 5 (int value) to 'c' variable;
-
+c=5
 # Print the sum of three variables 'a', 'b', 'c';
+x=a+b+c
+print(x)
